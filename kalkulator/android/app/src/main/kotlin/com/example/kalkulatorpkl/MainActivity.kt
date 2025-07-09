f58f43ea9d18cc9f2b5048aa68b9d99509adde96
Raynaldo <dor@example.com>
2025-07-09 09:07:54 +0700

    rapiin repo

--- kalkulator/android/app/src/main/kotlin/com/example/kalkulatorpkl/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kalkulatorpkl
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- slicing/android/app/src/main/kotlin/com/example/slicing/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.slicing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
