f9dd5ddcc2585d0e67fa8ba0757dac3d27f633a6
Raynaldo <dor@example.com>
2025-07-09 09:44:12 +0700

    masih benerin repo

--- kalkulator/lib/app/modules/home/views/home_view.dart
+++ kalkulator/lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
-import 'package:math_expressions/math_expressions.dart';
+
 class CalculatorHomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
--- kalkulator/lib/app/modules/splash/controllers/splash_controller.dart
+++ kalkulator/lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import '../../../routes/app_pages.dart';
 import '../../../routes/app_routes.dart';
 
 class SplashController extends GetxController {
